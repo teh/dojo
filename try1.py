adb29a1d170b076be9e6da7b0791ca98a44c05d6
Thomas Hunger <tehunger@gmail.com>
2012-10-04 21:00:03 +0100

    oh hai

---
+++ try1.py
@@ -0,0 +1,49 @@
+import random
+import itertools
+import math
+
+def generate_expressions(numbers):
+    
+    for a, b, c, d, e in itertools.permutations(numbers):
+        for k, l, m, n in itertools.product(*([['+', '-', '*', '/']] * 4)):
+            yield '%f %s %f %s %f %s %f %s %f' % (a, k, b, l, c, m, d, n, e)
+
+
+
+def main():
+    BigNum = 2
+    OtrNum = 3
+    numbers = (
+        [random.randrange(1, 11) for i in xrange(OtrNum)]
+        +[random.choice([25,50,75,100]) for i in xrange(BigNum)]
+    )
+    target = random.randrange(100, 1000)
+
+    #    closeness(eval(generate_expression(numbers)), target)
+
+    print "Numbers" ,numbers
+    print "Target", target
+
+    results = []
+    best = target
+    bestexp = ""
+    for i in generate_expressions(numbers):
+	try:
+            guess = eval(i)
+        except ZeroDivisionError:
+            continue
+
+	if math.trunc(guess) != guess:
+            continue
+
+        diff = abs(guess - target)
+        if diff < best:
+	    best = diff      
+            bestexpr = i
+        #print '%s = %s' % (i, guess)
+    print "\n\n"
+    print bestexpr, "=", target, "difference = ", int(best) 
+
+if __name__ == '__main__':
+    main()
+    
